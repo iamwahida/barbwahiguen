5b37694eff5e29945b2f251e840b30f074f214b1
iamwahida <nemati.w@yahoo.com>
2024-05-17 17:44:20 +0200

    movieentity methods added

--- src/main/java/at/ac/fhcampuswien/fhmdb/database/MovieEntity.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/database/MovieEntity.java
@@ -5,7 +5,9 @@ import at.ac.fhcampuswien.fhmdb.models.Movie;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 //In this class we establish the DB Table that saves movie brought in through the remote API
 
@@ -33,17 +35,52 @@ public class MovieEntity {
     //A default constructor is needed so that the object can be returned from a DB query or so that a table can be created
     public MovieEntity(){};
 
-    String genresToString(List<Genre> genres){
-        return this.genres;
-    }
-
-    List <MovieEntity> fromMovies(List <Movie> movies){
+    public static List <MovieEntity> fromMovies(List <Movie> movies){
         List <MovieEntity> listMovieEntities = new ArrayList<>();
+        for(Movie movie : movies){
+            listMovieEntities.add(convertToMovieEntity(movie));
+        }
         return listMovieEntities;
     }
 
-    List <Movie> toMovies(List <MovieEntity> movies){
+    public static List <Movie> toMovies(List <MovieEntity> movieEntities){
         List <Movie> listMovies = new ArrayList<>();
+        for(MovieEntity movieEntity : movieEntities){
+            listMovies.add(convertToMovie(movieEntity));
+        }
         return listMovies;
+    }
+
+    public static MovieEntity convertToMovieEntity(Movie movie){
+        MovieEntity movieEntity = new MovieEntity();
+        movieEntity.apiId = movie.getApiId();
+        movieEntity.title = movie.getTitle();
+        movieEntity.description = movie.getDescription();
+        movieEntity.genres = convertGenresToString(movie.getGenres());
+        movieEntity.releaseYear = movie.getReleaseYear();
+        movieEntity.imgUrl = movie.getImgUrl();
+        movieEntity.lengthInMinutes = movie.getLengthInMinutes();
+        movieEntity.rating = movie.getRating();
+        return movieEntity;
+    }
+
+    public static String convertGenresToString(List <Genre> genres){
+        return genres.stream().map(Enum::name).collect(Collectors.joining(", "));
+    }
+
+    public static Movie convertToMovie(MovieEntity movieEntity){
+        return new Movie(
+                String.valueOf(movieEntity.apiId),
+                movieEntity.title,
+                movieEntity.description,
+                convertStringToGenre(movieEntity.genres),
+                movieEntity.releaseYear,
+                movieEntity.imgUrl,
+                movieEntity.lengthInMinutes,
+                movieEntity.rating);
+    }
+
+    public static List <Genre> convertStringToGenre(String genres){
+        return Arrays.stream(genres.split(",\\s*")).map(Genre::valueOf).collect(Collectors.toList());
     }
 }
